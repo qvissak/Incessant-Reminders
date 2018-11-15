e82babf2aeeeeecb993d28893c02d45ebb91f533
qvissak <quinn.vissak@uconn.edu>
2018-11-15 00:03:20 -0500

    Add frequency error message

--- src/components/Landing/ReminderFrequency.js
+++ src/components/Landing/ReminderFrequency.js
@@ -6,16 +6,19 @@ import './styles.css'
 class ReminderFrequency extends React.Component {
   render = () => {
     const { updateTime, reminderFrequencyMinutes } = this.props
+    const incorrectInput = reminderFrequencyMinutes <= 0
+    const errorMessage = 'Please enter a number greater than 0.'
+    const directions = 'How frequently (in minutes) do you want to be reminded?'
 
     return <TextField
       label="Reminder Frequency"
       value={reminderFrequencyMinutes}
       onChange={updateTime}
-      type="number"
       InputLabelProps={{
         shrink: true
       }}
-      helperText="How frequently (in minutes) do you want to be reminded?"
+      error={incorrectInput}
+      helperText={incorrectInput ? errorMessage : directions}
       margin="none"
       variant="outlined"
     />
