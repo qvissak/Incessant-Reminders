f200e417cf801ff77c8ca725cf3f19b2336bcf87
Arun George <11051042+aruniverse@users.noreply.github.com>
2020-10-22 22:35:07 -0400

    bump to latest react so we can use hooks, even though we have yet to do so. converted some class based components to functional ones (#12)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,15 +5,16 @@ import Typography from '@material-ui/core/Typography'
 
 import './styles.css'
 
-class Header extends React.Component {
-  render = () =>
+const Header = () => {
+  return (
     <AppBar color="default">
       <Toolbar variant="dense">
         <Typography variant="subtitle1" color="inherit">
-            Incessant Reminders
+          Incessant Reminders
         </Typography>
       </Toolbar>
     </AppBar>
+  );
 }
 
 export default Header
--- src/components/Landing/ReminderFrequency.js
+++ src/components/Landing/ReminderFrequency.js
@@ -3,14 +3,13 @@ import TextField from '@material-ui/core/TextField'
 
 import './styles.css'
 
-class ReminderFrequency extends React.Component {
-  render = () => {
-    const { updateTime, reminderFrequencyMinutes } = this.props
-    const incorrectInput = reminderFrequencyMinutes <= 0
-    const errorMessage = 'Please enter a number greater than 0.'
-    const directions = 'How frequently (in minutes) do you want to be reminded?'
+const ReminderFrequency = ({ updateTime, reminderFrequencyMinutes }) => {
+  const incorrectInput = reminderFrequencyMinutes <= 0
+  const errorMessage = 'Please enter a number greater than 0.'
+  const directions = 'How frequently (in minutes) do you want to be reminded?'
 
-    return <TextField
+  return (
+    <TextField
       label="Reminder Frequency"
       value={reminderFrequencyMinutes}
       onChange={updateTime}
@@ -22,7 +21,7 @@ class ReminderFrequency extends React.Component {
       margin="none"
       variant="outlined"
     />
-  }
+  );
 }
 
 export default ReminderFrequency
